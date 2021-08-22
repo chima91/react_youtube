c2ca41ee2b327f195b13a2a75b2dbbc8902e6616
Kazuma NAGAI <itb4769we@gmail.com>
2021-08-22 20:04:48 +0900

    setting of material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
+import { CssBaseline, createTheme, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RootRouter } from './Route';
 
+const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <RootRouter />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
+        <RootRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
