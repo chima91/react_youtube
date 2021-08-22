3dda1570add391f8a32d965671d23fb976e251a6
Kazuma NAGAI <itb4769we@gmail.com>
2021-08-22 20:55:35 +0900

    complete header

--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,6 +1,8 @@
-import { AppBar, IconButton, Toolbar } from "@material-ui/core";
+import { AppBar, Avatar, IconButton, Toolbar } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import VideoCallIcon from "@material-ui/icons/VideoCall"
 import { Logo } from "../../components/Logo";
+import { SearchBar } from "./SearchBar";
 
 import useStyles from "./style";
 
@@ -10,13 +12,26 @@ export const DashboardHeader = () => {
   return(
     // color: 背景を白に, elevation: box-shadowをなくす
     <AppBar elevation={0} color="inherit">
-      <Toolbar>
-        <IconButton>
-          <MenuIcon />
-        </IconButton>
-        {/* useStylesの値は、CSSモジュールと全く同じような使い方で使用できる */}
-        <div className={styles.logo}>
-          <Logo/>
+      <Toolbar className={styles.between}>
+        <div className={styles.flex}>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          {/* useStylesの値は、CSSモジュールと全く同じような使い方で使用できる */}
+          <div className={styles.logo}>
+            <Logo/>
+          </div>
+        </div>
+
+        <SearchBar />
+
+        <div className={styles.flex}>
+          <IconButton>
+            <VideoCallIcon />
+          </IconButton>
+          <IconButton className={styles.profileIcon}>
+            <Avatar />
+          </IconButton>
         </div>
       </Toolbar>
     </AppBar>
