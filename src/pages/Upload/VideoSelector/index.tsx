efcc3e320701bf48639e77c59b8ebeb675cbae0c
Kazuma NAGAI <itb4769we@gmail.com>
2021-08-23 23:25:09 +0900

    create video-upload URL.createObjectURL

--- src/pages/Upload/VideoSelector/index.tsx
+++ src/pages/Upload/VideoSelector/index.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@material-ui/core";
+import { Button, CardMedia } from "@material-ui/core";
+import { useEffect } from "react";
 import { ChangeEvent, useState } from "react";
 import { useRef } from "react";
 
@@ -15,8 +16,26 @@ export const VideoSelect = () => {
     }
   }
 
+  const [videoURL, setVideoURL] = useState<string>();
+  // useEffectは、第2引数に指定した変数が変更されたら、第1引数の関数を実行します。
+  useEffect(() => {
+    if(file) {
+      // URL.createObjectURLは、ファイルを引数に受け取り、<video>タグで読み込み可能なローカルURLを生成します。
+      // URL.createObjectURLで生成されたURLを<video>のsrcにわたすことでファイルを動画で表示できます。
+      setVideoURL(URL.createObjectURL(file));
+    }
+  }, [file]);
+
   return (
     <div>
+      {/*
+        これは、React流の`if文`です。
+        if(videoURL){<CardMedia />}と同じ意味を成します。
+        しかし、JSX内では`if文`が使用できないため、このような特殊な書き方をしています。
+        この書き方を使用するときは、この部分の全体を"{}"で囲むことをお忘れなく。
+      */}
+      {videoURL && <CardMedia component="video" controls src={videoURL} />}
+
       {/*
         <input>タグをボタンの上に配置する。
         今回は、<Button>をクリックしたらファイルを選択する使用にしたいので、<input type="file"/>という形で、インプットのタイプに'file'を指定します。
