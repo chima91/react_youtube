045eb6cd0a104d9573340fa2ba73efb6ed100da0
Kazuma NAGAI <itb4769we@gmail.com>
2021-08-28 12:31:18 +0900

    setting of firebase

---
+++ src/utils/Firebase/config.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTNGNi0hVGlipVOc7Ui3CWk-nu4Jgb4ss",
+  authDomain: "react-a88c9.firebaseapp.com",
+  projectId: "react-a88c9",
+  storageBucket: "react-a88c9.appspot.com",
+  messagingSenderId: "1055992332617",
+  appId: "1:1055992332617:web:2c15446340e3f1adc0da98"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const fireAuth = firebase.auth();
+export const storage = firebase.storage();
+export default firebase;
