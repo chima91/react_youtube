7e9f6047bdcf9b0abbc10fa61b5fea647075dd48
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-12 12:41:03 +0900

    add login process with useLogin()

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,21 +2,29 @@ import { Button, Card, TextField, Typography } from "@material-ui/core";
 
 import { Logo } from "../../components/Logo";
 import useStyles from "./style";
+import { useLogin } from "../../hooks/Authentication/useLogin";
 
 export const Login = () => {
   const styles = useStyles();
+  const { ref, error, loading, login } = useLogin();
+
   return (
-    // 全体を囲むCardコンポーネント
     <Card className={styles.root} variant="outlined">
-      {/* ロゴコンポーネント */}
+
       <div className={`${styles.logo} ${styles.margin}`}>
         <Logo />
       </div>
 
-      {/* タイトルコンポーネント */}
       <Typography className={styles.margin} component="h1" variant="h5">
         ログイン
       </Typography>
+
+      {/* エラーメッセージを表示 */}
+      {error.has("main") && (
+        <Typography className={styles.margin} color="error">
+          {error.get("main")}
+        </Typography>
+      )}
 
       {/* メールアドレスフィールド */}
       <label className={`${styles.label} ${styles.margin}`}>
@@ -27,6 +35,12 @@ export const Login = () => {
           size="small"
           fullWidth
           variant="outlined"
+          // useRefで作成したemailRefを渡してフォームの値を取得する。
+          inputRef={ref.emailRef}
+          // エラーがあれば、フォームのデザインをerror用に変更させる
+          error={error.has("email")}
+          // エラーの詳細のフォームの下部に表示する
+          helperText={error.has("email") ? error.get("email") : ""}
         />
       </label>
 
@@ -39,23 +53,34 @@ export const Login = () => {
           size="small"
           fullWidth
           variant="outlined"
+          // useRefで作成したpasswordRefを渡してフォームの値を取得する。
+          inputRef={ref.passwordRef}
+          // エラーがあれば、フォームのデザインをerror用に変更させる
+          error={error.has("password")}
+          // エラーの詳細のフォームの下部に表示する
+          helperText={error.has("password") ? error.get("password") : ""}
         />
       </label>
 
       {/* Submitボタン */}
       <div className={styles.margin}>
-        <Button variant="contained" color="primary">
-          ログイン
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={loading}
+          onClick={login}
+        >
+          {loading ? "Now Loading..." : "ログイン"}
         </Button>
       </div>
 
       <div>
-        <Button href="#link" color="primary">
+        <Button href="/signup" color="primary">
           アカウント作成はこちら
         </Button>
       </div>
       <div>
-        <Button href="#link" color="primary">
+        <Button href="/forget" color="primary">
           パスワードを忘れた場合はこちら
         </Button>
       </div>
