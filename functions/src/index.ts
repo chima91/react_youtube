789f5a5e28652083b9a3d18f332076e18cee5e64
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-18 13:13:51 +0900

    create custom claim for Hasura

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// firebaseの初期化
+admin.initializeApp(functions.config().firebase);
+
+// Firebase FunctionsにprocessSignUpという名前の関数を作成
+// `functions.auth.user().onCreate` により、Authenticationでuserが作成されたときに実行される関数を定義
+exports.processSignUp = functions.auth.user().onCreate(user => {
+  // Hasura用のカスタムクレームの作成
+  const customClaims = {
+    "http://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": user.uid,
+    }
+  };
+
+  // userのトークンにカスタムトークンを追加する
+  return admin.auth().setCustomUserClaims(user.uid, customClaims);
+})
