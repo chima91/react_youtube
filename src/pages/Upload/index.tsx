2d44e46a3d2cfeabec92bea90f647f06b1b9347c
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-20 12:17:32 +0900

    change process of /upload whether user is logged in or not

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -1,36 +1,60 @@
-import { Dialog, DialogTitle, DialogContent, Divider, Grid } from "@material-ui/core";
+import { Dialog, DialogTitle, DialogContent, Divider, Grid, CircularProgress } from "@material-ui/core";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 import { VideoSelect } from "./VideoSelector";
 import { UploadForm } from "./UploadForm";
 import useStyles from "./style";
+import { AccountLoaded } from "../../stores/AccountLoaded";
+import { GlobalUser } from "../../stores/User";
 
 export const Upload = () => {
   const styles = useStyles();
+
+  // recoilの値を使用
+  const accountLoaded = useRecoilValue(AccountLoaded);
+  const user = useRecoilValue(GlobalUser);
+
+  // react routerを使用
+  const navigate = useNavigate();
+
+  // アカウントが読み込まれていない、未ログインであれば、/login へリダイレクト
+  useEffect(() => {
+    if(accountLoaded) {
+      if(!user?.id) {
+        navigate("/login");
+      }
+    }
+  }, [accountLoaded, user?.id]);
 
   return (
     <Dialog fullWidth={true} maxWidth="md" open={true}>
       {/* タイトル用コンポーネント */}
       <DialogTitle>動画のアップロード</DialogTitle>
-      {/* 横線コンポーネント */}
+      {/* 横線コンポーネント（orientation="vertical" で縦線となる） */}
       <Divider />
 
       {/* コンテント用コンポーネント */}
       <DialogContent className={styles.body}>
-        <Grid container spacing={4}>
-          <Grid xs item>
-            <VideoSelect />
+        {/* アカウントが存在すれば、アップロードコンポーネントを表示 */}
+        {user?.id ? (
+          <Grid container spacing={4}>
+            <Grid xs item>
+              <VideoSelect />
+            </Grid>
+            <Divider orientation="vertical" flexItem />
+            <Grid xs item>
+              <UploadForm />
+            </Grid>
           </Grid>
-
-          {/*
-            真ん中に縦線を挿入
-            デザインにあるような線を入れるため
-          */}
-          <Divider orientation="vertical" flexItem />
-
-          <Grid xs item>
-            <UploadForm />
+        ) : (
+          // ローディングコンポーネントを表示
+          <Grid container justifyContent="center">
+            <CircularProgress size={50} />
           </Grid>
-        </Grid>
+        )
+        }
       </DialogContent>
     </Dialog>
   );
