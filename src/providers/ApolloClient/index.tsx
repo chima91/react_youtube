ae1a330d243a8730a42d5702743f2bbc455de5ff
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-20 10:59:49 +0900

    add token to headers of ApolloClient

--- src/providers/ApolloClient/index.tsx
+++ src/providers/ApolloClient/index.tsx
@@ -2,18 +2,25 @@ import { ApolloProvider as Provider, ApolloClient, createHttpLink, InMemoryCache
 import { setContext } from "@apollo/client/link/context";
 import { PropsWithChildren } from "react";
 
+import { fireAuth } from "../../utils/Firebase/config";
+
 // GraphQL APIのエンドポイントを指定する
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_END_POINT_ORIGIN
 });
 
 // GraphQLのリクエストを送信する際に付与するRequest Headerなどをここで指定する
-const authLink = setContext(() => {
-  return {
-    headers: {
-      "x-hasura-admin-secret": process.env.REACT_APP_HASURA_SECRET_KEY
-    }
-  }
+// setContext：ApolloLinkを生成。serverにリクエストを送信する前に、リクエストを改造できるやつ（ざっくり）
+// headers：HTTPリクエストのヘッダー。ヘッダーはリクエストに情報を付与できるデータ
+// Authorization：HTTP認証要求ヘッダー。HTTPヘッダーの中で、認証情報を格納するためのプロパティ。
+// Bearerトークン：OAuth2.0の認可機構。認証情報がどのような形で送られてくるかを明示するための規格。
+const authLink = setContext(async() => {
+  const token = await fireAuth.currentUser?.getIdToken(true);
+
+  // Bearerトークンでトークンを送信する
+  // headersのプロパティは`Authorization`
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
+  return { headers }
 });
 
 // ApolloClientのインスタンスをここで作成
