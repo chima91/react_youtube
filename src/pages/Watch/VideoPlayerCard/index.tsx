a50614ae0047e2905447e52fe6d7d33e193407a3
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-21 16:15:51 +0900

    add props to VideoPlayerCard

--- src/pages/Watch/VideoPlayerCard/index.tsx
+++ src/pages/Watch/VideoPlayerCard/index.tsx
@@ -1,57 +1,58 @@
 import { Avatar, Card, CardContent, CardHeader, CardMedia, Divider, Typography} from "@material-ui/core";
+import { useEffect, useState } from "react";
 
 import useStyles from "./style";
 
-export const VideoPlayerCard = () => {
+export type VideoPlayerCardProps = {
+  title: string | undefined;
+  description: string | undefined;
+  views: number | undefined;
+  ownerName: string | undefined;
+  date: Date | undefined;
+  fetcher: () => Promise<string | undefined>;
+};
+
+export const VideoPlayerCard = ({
+  title,
+  description,
+  views,
+  ownerName,
+  date,
+  fetcher,
+}: VideoPlayerCardProps) => {
   const styles = useStyles();
+
+  // 動画のダウンロードリンクを格納するためのステート
+  const[src, setSrc] = useState<string>();
+  useEffect(() => {
+    // firebase storageから動画のDLリンクを取得する
+    fetcher().then(setSrc);
+  })
 
   return (
     <Card className={styles.transparent} elevation={0} square>
-      {/* 動画プレイヤー表示エリア */}
-      {/*
-        CardMediaは、画像の他に動画,音声などのメディア系コンポーネントの作成もできます。
-        メディアの指定は、`component`というプロパティに指定のメディアコンポーネント`img`,`video`,`audio`などのHTMLタグを指定するだけです。
-        そして、`src`にメディアのパスを指定すると画面に表示されます。
-        そして、今回はビデオプレイヤーに操作用のコントローラーを表示させたいので、`controls`というプロパティを指定しています。
-        (`controls`はMaterial-UI特有のプロパティではなく、<video>HTMLタグのプロパティです。)
-      */}
-      <CardMedia component="video" controls src="/static/production ID_4763824.mp4" />
+      {/*　CardMediaは、画像の他に動画,音声などのメディア系コンポーネントの作成も可能。メディアの指定は、`component`というプロパティに`img`,`video`,`audio`などのHTMLタグを指定するだけ。　*/}
+      <CardMedia component="video" controls src={src} />
 
-      {/* タイトル表示エリア */}
       <CardContent className={styles.paddingHorizontalLess}>
         {/*
-          `Typography`コンポーネントは、テキストコンポーネントを簡単に作ることができます。
-          今回、componentには`h2`を、`variant`には`h6`を指定しています。
-          これは、HTMLタグは`<h2>`を使いスタイリングは、Material-UIで用意されているh6用のスタイリングを使うよう指示しています。
-          <h2>タグ使いたいけど、フォントサイズなどはh6でのサイズを使いたい場合などに便利です。
+          `Typography`コンポーネントで、テキストコンポーネントを簡単に作ることが可能。今回、componentには`h2`を、`variant`には`h6`を指定している。
+          これは、HTMLタグは`<h2>`を使い、スタイリングはMaterial-UIで用意されているh6用のスタイルを使うよう指定。<h2>タグ使いたいけど、フォントサイズなどはh6でのサイズを使いたい場合などに便利。
         */}
-        <Typography component="h2" variant="h6">
-          Organization Admin Settings: Dashboard overview [1/7]
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          10,094,526 回視聴 • 2018/08/06
-        </Typography>
+        <Typography component="h2" variant="h6">{title}</Typography>
+        <Typography variant="body2" color="textSecondary">{views} 回視聴・{date? new Date(date).toLocaleDateString() : ''}</Typography>
       </CardContent>
 
-      {/* タイトル下の横線 */}
       <Divider />
 
-      {/* 投稿者情報エリア */}
       <CardHeader
         className={styles.paddingHorizontalLess}
         avatar={<Avatar />}
-        title="Movieclips Trailers"
-        subheader="104K subscribers"
+        title={ownerName}
+        subheader="0 subscribers"
       />
 
-      {/* 説明文エリア */}
-      <CardContent className={styles.descPadding}>
-        Find your absolutely beautiful and serene place and listen to nature
-        sounds, birds signing and relaxing water sounds with breathtaking views
-        of Mount Shuksan. It’s 8-hour 4k video of discovery and peace. Download
-        it for your personal use and transform your 4K TV into a source of
-        relaxation and restoration.
-      </CardContent>
+      <CardContent className={styles.descPadding}>{description}</CardContent>
     </Card>
   );
 };
