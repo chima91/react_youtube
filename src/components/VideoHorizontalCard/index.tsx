cf23a8427a6c5934f85ce518e6797f307d205d01
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-21 16:26:44 +0900

    add props to VideoHorizontalCard

--- src/components/VideoHorizontalCard/index.tsx
+++ src/components/VideoHorizontalCard/index.tsx
@@ -1,28 +1,38 @@
 import { Card, CardHeader, CardMedia } from "@material-ui/core";
+import { useEffect, useState } from "react";
 
-import { HeaderTitle } from "../VideoCard/HeaderTitle";
-import { SubHeaderContent } from "../VideoCard/SubHeaderContent";
+import { HeaderTitle, HeaderTitleProps } from "../VideoCard/HeaderTitle";
+import { SubHeaderContent, SubHeaderContentProps } from "../VideoCard/SubHeaderContent";
 import useStyles from "./style";
 
-export const VideoHorizontalCard = () => {
+// 親コンポーネントから渡されるpropsの型。インターセクション型による型定義
+export type VideoHorizontalCardProps = {
+  fetcher: () => Promise<string | undefined>;
+} & HeaderTitleProps & SubHeaderContentProps;
+
+export const VideoHorizontalCard = ({title, owner, views, created, fetcher}: VideoHorizontalCardProps) => {
   const styles = useStyles();
+
+  // サムネイルのダウンロードリンクのステート
+  const [src, setSrc] = useState<string>();
+  useEffect(() => {
+    // サムネのダウンロードリンクを取得する関数
+    fetcher().then(setSrc);
+  })
 
   return (
     // 複数のスタイルを適用したい場合、${}`という記法を用いることで、変数の値を文字として展開する
-    // elevation={0} : box-shadowの影を削除する
-    // square: border-radiusを削除する
+    // elevation={0}: box-shadowの影を削除する。square: border-radiusを削除する
     <Card className={`${styles.root} ${styles.transparent}`} elevation={0} square>
-      {/*
-        サムネイル用のメディアコンポーネントを作成
-      */}
       <div className={styles.thumbnail}>
-        <CardMedia className={styles.media} image="/static/no-image.jpg" title="Thumbnail" />
+        <CardMedia className={styles.media} image={src} title="Thumbnail" />
       </div>
 
-      {/*
-        `Home`で作成した<HeaderTitle>と<SubHeaderContent>を流用する
-      */}
-      <CardHeader className={styles.contentPadding} title={<HeaderTitle />} subheader={<SubHeaderContent />} />
+      <CardHeader
+        className={styles.contentPadding}
+        title={<HeaderTitle title={title} />}
+        subheader={<SubHeaderContent owner={owner} views={views} created={created} />}
+      />
     </Card>
   );
 };
