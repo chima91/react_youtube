2330c5dbf6fb88b3c3f4878a8d1126b3bafc8925
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-21 15:10:08 +0900

    add props to SubHeaderContent of VideoCard

--- src/components/VideoCard/SubHeaderContent/index.tsx
+++ src/components/VideoCard/SubHeaderContent/index.tsx
@@ -1,10 +1,21 @@
 import { Typography } from "@material-ui/core";
+import { create } from "domain";
 
-export const SubHeaderContent = () => {
+export type SubHeaderContentProps = {
+  owner: string;
+  views: number;
+  created: Date;
+}
+
+export const SubHeaderContent = ({
+  owner,
+  views,
+  created
+}: SubHeaderContentProps) => {
   return (
     <>
-      <Typography variant="body2">Figma</Typography>
-      <Typography variant="body2">16K views 2 months ago</Typography>
+      <Typography variant="body2">{owner}</Typography>
+      <Typography variant="body2">{views} 回視聴されています {new Date(created).toLocaleDateString()}</Typography>
     </>
   );
 };
